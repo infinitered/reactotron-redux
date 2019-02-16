4ae4731a49719fa7e14f7b2a290543afa715ed6f
Richard Evans <rmevans9@gmail.com>
2019-02-16 13:57:20 -0500

    Cleanup the code a little bit

--- src/customDispatch.ts
+++ src/customDispatch.ts
@@ -17,22 +17,17 @@ export default function createCustomDispatch(
 
     var unwrappedAction = action.type === "PERFORM_ACTION" && action.action ? action.action : action
 
-    // if matchException is true, actionType is matched with exception
-    const matchException = (exception, actionType) => {
+    const isException = exceptions.some(exception => {
       if (typeof exception === "string") {
-        return actionType === exception
+        return unwrappedAction.type === exception
       } else if (typeof exception === "function") {
-        return exception(actionType)
+        return exception(unwrappedAction.type)
       } else if (exception instanceof RegExp) {
-        return exception.test(actionType)
+        return exception.test(unwrappedAction.type)
       } else {
         return false
       }
-    }
-
-    const isException = exceptions.some(exception =>
-      matchException(exception, unwrappedAction.type)
-    )
+    })
 
     // action not blacklisted?
     // if matchException is true, action.type is matched with exception
