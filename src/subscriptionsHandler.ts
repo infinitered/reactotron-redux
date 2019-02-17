a1728dd68678fd490efd14e6abcedd604560f26b
Richard Evans <rmevans9@gmail.com>
2019-02-17 13:16:29 -0500

    Subscriptions appear to be working

--- src/subscriptionsHandler.ts
+++ src/subscriptionsHandler.ts
@@ -2,24 +2,55 @@ import pathObject from "./helpers/pathObject"
 
 export default function createSubscriptionHandler(reactotron: any) {
   const reduxStore = reactotron.reduxStore
-  let subscriptions = []
+  let subscriptions: string[] = []
 
   function setSubscriptions(subs: string[]) {
     subscriptions = subs
   }
 
-  function sendSubscriptions() {
+  function getChanges() {
     const state = reduxStore.getState()
-    const changes = subscriptions.map(sub => pathObject(sub, state))
+
+    const changes = []
+
+    subscriptions.forEach(path => {
+      let cleanedPath = path
+      let starredPath = false
+
+      if (path.endsWith("*")) {
+        // Handle the star!
+        starredPath = true
+        cleanedPath = path.substr(0, path.length - 2)
+      }
+
+      const values = pathObject(cleanedPath, state)
+
+      if (starredPath && cleanedPath) {
+        changes.push(
+          ...Object.entries(values).map(val => ({
+            path: `${cleanedPath}.${val[0]}`,
+            values: val[1],
+          }))
+        )
+      } else {
+        changes.push({ path: cleanedPath, values })
+      }
+    })
+
+    return changes
+  }
+
+  function sendSubscriptions() {
+    const changes = getChanges()
     reactotron.stateValuesChange(changes)
   }
 
   function sendSubscriptionsIfNeeded() {
-    // const state = reduxStore.getState()
-    // const changes = subscriptions.map(sub => pathObject(sub, state))
-    // if (changes.length < -1) {
-    //     reactotron.stateValuesChange(changes)
-    // }
+    const changes = getChanges()
+
+    if (changes.length > 0) {
+      reactotron.stateValuesChange(changes)
+    }
   }
 
   reduxStore.subscribe(sendSubscriptionsIfNeeded)
